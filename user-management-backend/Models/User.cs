cc4584b309583acb8c332b5e7b46c0facd3532c9
agostondunszt <agostondunsztdev@gmail.com>
2026-01-06 10:33:02 +0100

    User osztály + projekt váz elkészítése

---
+++ user-management-backend/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace user_management_backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Gender { get; set; } =  string.Empty;
+}
